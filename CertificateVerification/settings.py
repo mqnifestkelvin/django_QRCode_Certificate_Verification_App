6a7f49dd508792c4a408320007db51dc4b14909b
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-10-17 18:20:36 +0100

    modified settings.py file

--- CertificateVerification/settings.py
+++ CertificateVerification/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY', default='default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=lambda v: [s.strip() for s in v.split(',')] if v else [])
 
 # Application definition
 
